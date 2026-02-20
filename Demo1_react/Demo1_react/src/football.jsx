9a2e92b1f9dc731e3e32af4106fccdc9e6a4357e
Santhosh <162867178+Santhoshyt123@users.noreply.github.com>
2026-02-20 15:44:02 +0530

    added another react

---
+++ Demo1_react/Demo1_react/src/football.jsx
@@ -0,0 +1,14 @@
+import app from "./App.jsx";
+
+function Football() { 
+    return (
+        <div>
+            <h1>Football</h1>
+            <p>Football is a popular sport played worldwide.</p>
+            <p>It is played between two teams of eleven players each.</p>
+            <p>The objective of the game is to score goals by getting the ball into the opposing team's net.</p>
+        </div>
+    );
+}
+
+export default Football; 
